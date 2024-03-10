14bcc7a83b1c446f72b7d7187cf945743163c3f9
desaisoham0 <146281556+desaisoham0@users.noreply.github.com>
2024-03-10 01:26:24 +0000

    update picture

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -22,7 +22,7 @@ import PropTypes from "prop-types";
  */
 import image from "../images/soham_image_1466.jpg";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText = "Exploring brooklyn bridge";
 
 const Home = ({ name, title }) => {
   return (
